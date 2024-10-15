e6b9962a7697782ed67374b36771ea21537a7521
ChronosWithA-K <milo.lee27@student.tamdistrict.org>
2024-10-15 09:57:12 -0700

    add legality checker

--- ChooseYourPath.java
+++ ChooseYourPath.java
@@ -1,5 +1,18 @@
+
+import java.util.Scanner;
+
 public class ChooseYourPath {
+
     public static void main(String[] args) {
-        System.out.println("hi how are you");
+        Scanner sc = new Scanner(System.in);
+    }
+
+    public static boolean inputLegalityChecker(String userInput) {
+        for (int i = 0; i < userInput.length(); i++) {
+            if ((Character.isLetter(userInput.charAt(i)) == false)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
