a4cc2e1cb20917db7a9e210997657c33f5003199
ChronosWithA-K <milo.lee27@student.tamdistrict.org>
2024-10-17 09:43:59 -0700

    add first dialogue

--- ChooseYourPath.java
+++ ChooseYourPath.java
@@ -5,11 +5,15 @@ public class ChooseYourPath {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("You are on a path in the woods. And at the end of that path is a cabin. And in the basement of that cabin is a prince(ss). You're here to slay him(her). If you don't, it will be the end of the world.");
+        }
     }
 
     public static boolean inputLegalityChecker(String userInput) {
         for (int i = 0; i < userInput.length(); i++) {
-            if ((Character.isLetter(userInput.charAt(i)) == false)) {
+            if ((Character.isDigit(userInput.charAt(i)) == false)) {
                 return false;
             }
         }
