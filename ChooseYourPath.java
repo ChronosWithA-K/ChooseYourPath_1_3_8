f5c93235e352ddbdce931e845b9f8b0c8d83e78e
Daemons <143678373+ChronosWithA-K@users.noreply.github.com>
2024-10-22 16:58:47 +0000

    add more choices

--- ChooseYourPath.java
+++ ChooseYourPath.java
@@ -4,28 +4,47 @@ public class ChooseYourPath {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String choice;
-        boolean symbol1 = false; // Why is this not used? It clearly is - 24:36
-        boolean symbol2 = false;
-        boolean symbol3 = false;
-
-        System.out.println("All inputs have to be an integer, with no other characters. They have to be one of the given options.");
+        boolean bodySymbol = false; // Why is this not used? It clearly is - 24:36
+        boolean bedSymbol = false;
+        boolean cabinetSymbol = false;
 
         while (true) {
-            System.out.println("You wake up in a roughly 10x10x10 feet cubed room. It is dimly lit. You see a misshapen lump in the corner. What do you want to do:");
+            // clearTerminal();
+            System.out.println(
+                    "All inputs have to be an integer, with no other characters. They have to be one of the given options.\n");
+            System.out.println(
+                    "You wake up in a roughly 10x10x10 feet cubed room. It is dimly lit. You see a misshapen lump in the corner.");
             System.out.println("(1) Look around more");
             System.out.println("(2) Investigate the lump");
 
             choice = sc.nextLine();
             if (inputLegalityChecker(choice) && (choice.equals("1") || choice.equals("2"))) {
                 if (choice.equals("1")) {
-                    System.out.println("There is a cabinet in the corner of the room. There is also a bed in the corner.");
+                    System.out.println(
+                            "There is a cabinet in the corner of the room. There is also a bed in the corner. A door is set in the wall.");
                 } else {
-                    System.out.println("You investigate the lump, which turns out to be a body. You find what appears to be a symbol fragment.");
-                    symbol1 = true;
+                    System.out.println(
+                            "You investigate the lump, which turns out to be a body. You find what appears to be a symbol fragment.");
+                    bodySymbol = true;
+
+                    System.out.println(
+                            "Now that you've investigated the body, you look around. There is a cabinet in the corner of the room. There is also a bed in the corner. A door is set in the wall.");
+                    System.out.println("(1) Investigate the bed");
+                    System.out.println("(2) Investgate the cabinet");
+                    System.out.println("(3) Investigate the door");
+                    choice = sc.nextLine();
+                    if (inputLegalityChecker(choice) && (choice.equals("1") || choice.equals("2")
+                            || choice.equals("3"))) {
+                        if (choice.equals("1")) {
+                            System.out.print(
+                                    "You investigate the bed. You find a small puzzle box. You shake it, and something rattles within. You spend some time on it, solve it, ");
+                            System.out.println("and another symbol fragment comes out.");
+                            bedSymbol = true;
+                        } else if (choice.equals("2")) {
+                            System.out.println("");
+                        }
+                    }
                 }
-            } else {
-                clearTerminal();
-                System.out.println("Invalid input. Please enter either '1' or '2'.");
             }
         }
     }
